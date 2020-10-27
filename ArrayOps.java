319f93b67c67b40b0842bd8f9ff5a128a3dd3685
vli21 <vli21@stuy.edu>
2020-10-27 00:33:00 -0400

    public static int[] sumRows(int[][] matrix) prints array

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,6 +1,5 @@
+ 
 public class ArrayOps {
-
-
   //takes an array of int, and returns a sum of the individual values in the array.
   public static int sum(int[] arr) {
     int sum=0;
@@ -9,6 +8,28 @@ public class ArrayOps {
     }
     return sum;
    }
-   
+
+  //largest() takes a non-empty array of int, and returns the largest element of the array. [The problem states a precondition, the array must not be empty,
+  //this means you do not have to account for this at all. You are allowed to assume it to be true. This means the prior problem affords you no such assumptions,
+  //and must work on ALL array of ints.]
+  public static  int largest(int[]arr) {
+    int biggest =0;
+    for (int i=0; i <arr.length; i++){
+      if (arr[i]> biggest){
+        biggest = arr[i];
+      }
+    }
+    return biggest;
+  }
+
+  //sumRows()` takes a 2D array of int, and returns a 1D array of int with each element being the sum of the individual rows of the 2D array.
+  public static int[] sumRows(int[][] matrix) {
+    int [] sums = new int [matrix.length];
+    for (int i=0; i< matrix.length; i++){
+        sums[i]=sum(matrix[i]);
+
+    }
+    return sums;
+  }
 
 }
